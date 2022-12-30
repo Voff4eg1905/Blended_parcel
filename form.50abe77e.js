9660e7bc67ad02ea64974740495397f849966e1c
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-12-30 18:20:45 +0000

    Deploying to gh-pages from @ Voff4eg1905/Blended_parcel@502d50f05b25e8b1877abba6f1d6822c33698304 🚀

---
+++ form.50abe77e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-contact-form"),t={};e.addEventListener("input",(function(e){var n=e.target,o=n.name,r=n.value;t[o]=r,console.log(t),localStorage.setItem("userInfo",JSON.stringify(t))}))}();
+//# sourceMappingURL=form.50abe77e.js.map
---
+++ form.5c426adf.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-contact-form"),t={};e.addEventListener("input",(e=>{const{name:o,value:n}=e.target;t[o]=n,console.log(t),localStorage.setItem("userInfo",JSON.stringify(t))}));
+//# sourceMappingURL=form.5c426adf.js.map
