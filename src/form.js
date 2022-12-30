6ff47e484b4b6e95a6f64cb0fcbf283bb0bc06e3
SvitlanaHonchar <snidlty@gmail.com>
2022-12-30 20:38:05 +0200

    1.0

--- src/form.js
+++ src/form.js
@@ -1,15 +1,27 @@
-const form = document.querySelector(".js-contact-form");
+const form = document.querySelector('.js-contact-form');
 // console.log(form);
-const userData = {};
+
+initPage();
 
 const onInput = event => {
-    const {name, value} = event.target;
-    // console.log(event.target);
-    // console.log(event.target.name);
-    // console.log(event.target.value);
-    userData[name] = value;
-    console.log(userData);
-    localStorage.setItem("userInfo", JSON.stringify(userData));
-}
+  const { name, value } = event.target;
+  let userData = localStorage.getItem('userInfo');
+  userData = userData ? JSON.parse(userData) : {};
+  userData[name] = value;
+  localStorage.setItem('userInfo', JSON.stringify(userData));
+};
 
-form.addEventListener("input", onInput);
+form.addEventListener('input', onInput);
+
+function initPage() {
+  let savedData = localStorage.getItem('userInfo');
+  if (!savedData) {
+    return;
+  }
+  savedData = JSON.parse(savedData);
+
+  Object.entries(savedData).forEach(([name, value]) => {
+    console.log(name, value);
+    form.elements[name].value = value;
+  });
+}
